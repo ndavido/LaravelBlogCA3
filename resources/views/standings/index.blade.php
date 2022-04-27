5e828cbaa2bedcf2e08182419bcedfba67f0589d
ndavido <D00239107@student.dkit.ie>
2022-04-27 20:44:53 +0100

    Changed css for Standings table

--- resources/views/standings/index.blade.php
+++ resources/views/standings/index.blade.php
@@ -26,32 +26,55 @@
         </a>
     </div>
 @endif
-
-<div class="table">
-    <table class="w3-table-all">
-        <thead>
+<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-                <th>Position</th>
-                <th>Driver</th>
-                <th>Nationality</th>
-                <th>Car</th>
-                <th>Points</th>
+                <th scope="col" class="px-6 py-3">
+                    Position
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Driver
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Nationality
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Car
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Points
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    <span class="sr-only">Edit</span>
+                </th>
             </tr>
         </thead>
         <tbody>
             @foreach ($standings as $table)
-            <tr class="width=25%">
-                <td>{{ $table->position }}</td>
-                <td>{{ $table->driver }}</td>
-                <td>{{ $table->car }}</td>
-                <td>{{ $table->nationality }}</td>
-                <td>{{ $table->points }}</td>
-               
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <td class="px-6 py-4">
+                    {{ $table->position }}
+                </td>
+                <td scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                    {{ $table->driver }}
+                </td>
+                <td class="px-6 py-4">
+                    {{ $table->car }}
+                </td>
+                <td class="px-6 py-4">
+                    {{ $table->nationality }}
+                </td>
+                <td class="px-6 py-4">
+                    {{ $table->points }}
+                </td>
+                <td class="px-6 py-4 text-right">
+                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                </td>
             </tr>
             @endforeach
         </tbody>
     </table>
-  
 </div>
 
 @endsection
