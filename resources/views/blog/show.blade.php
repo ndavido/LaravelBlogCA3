2fc6e501cfae23bca5d0afa433baa6c8193d4974
ndavido <D00239107@student.dkit.ie>
2022-05-01 17:14:50 +0100

    CSS for adding comments added

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -1,22 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="w-4/5 m-auto text-left">
-    <div class="py-15">
-        <h1 class="text-6xl">
-            {{ $post->title }}
-        </h1>
+    <div class="w-4/5 m-auto text-left">
+        <div class="py-15">
+            <h1 class="text-6xl">
+                {{ $post->title }}
+            </h1>
+        </div>
     </div>
-</div>
 
-<div class="w-4/5 m-auto pt-20">
-    <span class="text-gray-500">
-        By <span class="font-bold italic text-gray-800">{{ $post->user->name }}</span>, Created on {{ date('jS M Y', strtotime($post->updated_at)) }}
-    </span>
+    <div class="w-4/5 m-auto pt-20">
+        <span class="text-gray-500">
+            By <span class="font-bold italic text-gray-800">{{ $post->user->name }}</span>, Created on
+            {{ date('jS M Y', strtotime($post->updated_at)) }}
+        </span>
 
-    <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
-        {{ $post->description }}
-    </p>
-</div>
+        <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
+            {{ $post->description }}
+        </p>
+    </div>
 
-@endsection 
+    @if (Auth::check())
+        <!-- Add Comment -->
+        <div class="flex mx-auto items-center justify-center shadow-lg mt-56 mx-8 mb-4 max-w-lg">
+            <form class="w-full max-w-xl bg-white rounded-lg px-4 pt-2">
+                <div class="flex flex-wrap -mx-3 mb-6">
+                    <h2 class="px-4 pt-3 pb-2 text-gray-800 text-lg">Add a new comment</h2>
+                    <div class="w-full md:w-full px-3 mb-2 mt-2">
+                        <textarea class="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white"
+                            name="body" placeholder='Type Your Comment...' required></textarea>
+                    </div>
+                    <div class="w-full md:w-full flex items-start md:w-full px-3">
+                        <div class="flex items-start w-1/2 text-gray-700 px-2 mr-auto">
+                        </div>
+                        <div class="-mr-1">
+                            <input type='submit'
+                                class="bg-white text-gray-700 font-medium py-1 px-4 border border-gray-400 rounded-lg tracking-wide mr-1 hover:bg-gray-100"
+                                value='Post Comment'>
+                        </div>
+                    </div>
+            </form>
+        </div>
+    @endif
+@endsection
